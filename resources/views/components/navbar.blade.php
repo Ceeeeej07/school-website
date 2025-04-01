82d34a3952ddeddfd03e4e19d3d98f47d1f81412
Carl J <cjcsantoceldes07@gmail.com>
2025-04-02 00:42:53 +0800

    update

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <div class="bg-green-800 text-white">
+        <div class="container mx-auto px-2 py-2 flex items-center">
+            <p class="flex-none">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <div class="flex-grow text-right">
+                <a href="#" class=" hover:text-black">
+                    Student Portal
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-gray-300">
+        <nav class="flex container mx-auto px-4 py-4 items-center">
+            <!-- Logo -->
+            <a href="#" class="flex items-center">
+                <img src="{{ asset('storage/icons/ANHS-2.png') }}" alt="Logo" class="h-15 w-auto">
+            </a>
+
+            <!-- Navigation Links -->
+            <div class="space-x-4 ml-4">
+                <a href="#" class="hover:text-white">Home</a>
+                <a href="#" class="hover:text-white">About</a>
+                <a href="#" class="hover:text-white">Academics</a>
+                <a href="#" class="hover:text-white">Admissions</a>
+                <a href="#" class="hover:text-white">News & Events</a>
+            </div>
+        </nav>
+    </div>
+
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antipas National High School</title>
+    <link rel="shortcut icon" href="{{ asset('storage/icons/ANHS.png') }}" type="image/x-icon">
+
+    <!-- Scripts -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+    <x-navbar />
+
+</body>
+</html>
